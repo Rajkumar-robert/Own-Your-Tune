e0b4bd068860e569158b0755094e161df4d43374
krishnan74 <divyakrishnan.r74@gmail.com>
2023-10-29 12:17:03 +0530

    ui2

--- client/src/components/MusicCard.jsx
+++ client/src/components/MusicCard.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import "./music-card.css";
 
@@ -23,11 +22,29 @@ const MusicCard = (props) => {
     >
       <div
         className="game-details-div
-         px-5 w-[321px] border-t-1 h-[321px]  bg-[rgba(255,255,255,0.4)] relative top-[321px] "
+         px-5 w-[321px] border-t-1 h-[321px]  bg-[rgba(0,0,0,0.4)] text-white relative top-[321px] "
       >
-        <h2 className="text-3xl font-extrabold text-black mt-3">
+        <h2 className="text-3xl font-extrabold text-white mt-3">
           {props.title}
         </h2>
+
+        {showDetails && (
+          <div className="text-xl">
+            <p>Song Genre: {props.songGenre}</p>
+            <p>Song Artist: {props.songArtist}</p>
+            <p>Song Price: ${props.songPrice}</p>
+            <p>Artist Address: {props.artistAddress}</p>
+
+            <div className="mt-3">
+              <button className="bg-blue-500 text-white px-5 py-1 rounded mr-2">
+                Buy
+              </button>
+              <button className="bg-green-500 text-white px-3 py-1 rounded">
+                Wishlist
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
